779f3c21f905961a16d84132c3a70e4a02df652f
Disfinder Root <3998461+disfinder@users.noreply.github.com>
2021-02-16 19:32:12 -0500

    install ansible via brew

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-echo "Installing Ansible..."
-pip install ansible
-
 echo "Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing Ansible..."
+brew install ansible
 
 ./playbook-init.yml
