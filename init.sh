d5801ed62c87a689bdaba1e71380689821e8868a
Disfinder Root <3998461+disfinder@users.noreply.github.com>
2024-01-22 18:35:43 -0500

    feat: update zsh (default macos shell) after installing brew

--- init.sh
+++ init.sh
@@ -3,6 +3,10 @@
 echo "Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+
+(echo; echo 'eval "\$(${HOMEBREW_PREFIX}/bin/brew shellenv)"') >> /Users/$USER/.zprofile
+eval "\$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
+
 echo "Installing Ansible..."
 brew install ansible
 
